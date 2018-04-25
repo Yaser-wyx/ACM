c79cf1c9d759cebaafc4a296d40389f666a18d40
wanyu <335767798@qq.com>
2018-04-25 08:58:04 +0800

    NOIP

--- Test.cpp
+++ Test.cpp
@@ -3,31 +3,26 @@
 #include <bits/stdc++.h>
 
 typedef long long int lli;
-#define data1  "E:\\WorkSpace\\ACM\\data.in"
+#define data1  "W:\\Workspace\\data.in"
 #define data2  "F:\\ACM\\data.in"
 #define INF 0xfffffff
 #define infinitesimal  -2100000000
 using namespace std;
 #define in fstream _file;_file.open(data1,ios::in);if(_file){freopen(data1,"r",stdin);}else{_file.open(data2,ios::in);if(_file)freopen(data2,"r",stdin);}
 #define mset(t, x) memset(t,x,sizeof(t))
+#define lson index<<1
+#define rson (index<<1) +1
 #define loop(a, b, c) for(int a=b;a<=c;a++)
 #define loop2(a, b, c) for(int a=b;a>=c;a--)
 #define loop3(a, b, c) for(int a=b;a<c;a++)
 #define loop4(a, b, c) for(int a=b;a>c;a--)
-#define loop5(a, b, c, d) for(int a=b;a<=c;a++)if(d)break;
-#define maxn 2005
+#define maxn 100
 #define maxm 20
 int n, m, t;
-char words[maxn][80];
-int dp[maxn];
+
 
 int main() {
     in;
-    scanf("%d", &n);
-    getchar();
-    loop(i, 1, n) {
-        scanf("%s", words[i]);
-    }
 
     return 0;
 }
